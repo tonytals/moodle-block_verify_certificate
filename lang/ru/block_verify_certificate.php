32409db19d0e73779a083042edfb81b106fd383a
Pavel Pisklakov <pavelpisklakov@Pavels-MacBook-Pro.local>
2013-11-11 21:43:17 +0600

    Added Russian Translation
    Added translation of block into Russian language

---
+++ lang/ru/block_verify_certificate.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['title'] = 'Проверить сертификат';
+$string['pluginname'] = 'Проверка сертификатов';
+$string['certificate'] = 'Проверка сертификата по коду:';
+$string['verifycertificate'] = 'Проверить сертификат';
+$string['notfound'] = 'Код сертификата не найден в базе. Подлинность сертификата невозможно установить.';
+$string['to'] = 'Выдан';
+$string['course'] = 'Курс';
+$string['date'] = 'Дата';
+$string['grade'] = 'Оценка';
