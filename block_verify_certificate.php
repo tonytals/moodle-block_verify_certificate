912813a37f5ac6563d7f4ff0b20c0e3b370661ef
Tony Galvão <webtals@gmail.com>
2015-04-16 16:20:03 -0300

    Update block_verify_certificate.php
    Novo formulário de pesquisa

--- block_verify_certificate.php
+++ block_verify_certificate.php
@@ -18,32 +18,44 @@
 
 class block_verify_certificate extends block_base {
 
-    public function init() {
+    function init() {
         $this->title = get_string('title', 'block_verify_certificate');
     }
 
-    public function applicable_formats() {
+    function applicable_formats() {
         return array('all' => true);
     }
-    public function get_content() {
+    function get_content() {
 
         if ($this->content !== null) {
             return $this->content;
         }
 
         $this->content = new stdClass;
-        $this->content->text = '<p>'.get_string('entercode', 'certificate').'</p>';
+        //$this->content->text = '<p>'.get_string('entercode', 'certificate').'</p>';
         $url = new moodle_url('/blocks/verify_certificate/index.php');
         $this->content->text .= '<center><form class="loginform" name="cert" method="post" action="'. $url . '">';
-
+        $this->content->text .= 'Por nome <input type="radio" name="por" id=name="porNome" value="nome" checked />'; // Para pesquisa por nome
+        $this->content->text .= 'Por código <input type="radio" name="por" id=name="porCodigo" value="code"/>'; // Para pesquisa por codigo
         $this->content->text .= '<input type="text" name="certnumber" id=name="certnumber" size="10" value="" />';
-        $this->content->text .= '<input type="submit" value="'.get_string('validate', 'certificate').'"/></form>';
+        $this->content->text .= '<input type="submit" value="Verificar"/></form>';
         $this->content->text .= '<center>';
         $this->content->footer = '';
+   
+        /**************** ANTIGO FORMULÁRIO ********************
+      
+        $this->content->text .= '<center><form class="loginform" name="cert" method="post" action="'. $url . '">';
+        $this->content->text .= '<input type="text" name="certnumber" id=name="certnumber" size="15" value="" />';
+        //$this->content->text .= '<input type="submit" value="'.get_string('validate', 'certificate').'"/></form>';
+        $this->content->text .= '<input type="submit" value="Verificar"/></form>';
+        $this->content->text .= '<center>';
+        $this->content->footer = '';
+      
+        */
 
         return $this->content;
     }
-    public function instance_allow_config() {
+    function instance_allow_config() {
         return false;
     }
 
